7bcf1fb717a3dc59638c0d80c5bcf0261744f8b6
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-01-13 16:05:33 +0000

    starter code

---
+++ Pet.java
@@ -0,0 +1,24 @@
+public class Pet {
+
+    // 1. Declare INSTANCE VARIABLES
+    private String name;
+    private int age;
+    private double weight;
+    private String type;
+    private boolean sterile;
+
+    // 2. Define CONSTRUCTOR
+    public Pet(String n, int a ) {
+        this.name = n;
+        this.age = a;
+        // keep going!
+    }
+
+    // 3. Define METHODS
+    // getters
+    // toString
+    // setters
+
+
+
+}
